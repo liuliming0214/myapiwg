2b1e1bc11234d1f551c9b711729c5ddfcd40feb3
liuliming0214 <277631488@qq.com>
2022-01-20 20:28:03 +0800

    Create cachekey.lua

---
+++ conf/cachekey.lua
@@ -0,0 +1,12 @@
+local config = {};
+config['wg_method'] 		= "wg_method_" -- 本地缓存key
+config['wg_application'] 	= "wg_application_" -- redis相关应用key
+config['wg_app_id'] 		= "wg_app_id_" -- redis相关应用key
+
+config['wg_app_time'] 		= 60 -- 配置相关keys的 本地缓存时间
+return config;
+
+
+
+
+
